ba069cce1528847bd98aa6f4b8794acf3680ae23
jaehyun <hyun18_@naver.com>
2022-11-05 18:48:41 +0900

    오류 수정 및 id 리네이밍

--- app/src/main/java/com/example/koing/AuthActivity.kt
+++ app/src/main/java/com/example/koing/AuthActivity.kt
@@ -1,11 +1,17 @@
 package com.example.koing
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.koing.databinding.ActivityAuthBinding
+import com.example.koing.databinding.ActivityMainBinding
 
 class AuthActivity : AppCompatActivity() {
+    lateinit var binding : ActivityAuthBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/koing/MainActivity.kt
+++ app/src/main/java/com/example/koing/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.koing
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.koing.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var context = this@MainActivity
+
+
     }
 }
