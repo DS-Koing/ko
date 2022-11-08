932d180ee1f0e861111304576d2f086b0372fd52
jiho-par <hkp314518@duksung.ac.kr>
2022-11-08 13:57:57 +0900

    MainActivity에서 Json으로 받아와 작성하던 부분은 삭제 (프론트 담당)

--- app/src/main/java/com/example/koing/MainActivity.kt
+++ app/src/main/java/com/example/koing/MainActivity.kt
@@ -20,21 +20,6 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
     lateinit var sharedPreferences: SharedPreferences
 
-    var getManagerApiBtn: Button? = null
-    var setManagerApiBtn: Button? = null
-
-    var userRetrofitInterface: UserRetrofitInterface? = null
-    var menuRetrofitInterface: MenuRetrofitInterface? = null
-    var s_menuRetrofitInterface: S_MenuRetrofitInterface? = null
-    var orderRetrofitInterface: OrderRetrofitInterface? = null
-    var cartRetrofitInterface: CartRetrofitInterface? = null
-
-    var callUser: Call<User>? = null
-    var callCart: Call<Cart>? = null
-    var callMenu: Call<Menu>? = null
-    var callSMenu: Call<S_Menu>? = null
-    var callOrder: Call<Order>? = null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,36 +34,6 @@ class MainActivity : AppCompatActivity() {
         // if 로그인 x
         val intent = Intent(context, AuthActivity::class.java)
         startActivity(intent)
-
-        //백엔드(박지호)
-        var retrofitClient: RetrofitClient? = RetrofitClient.getInstance()
-        var userRetrofitInterface = RetrofitClient.getUserRetrofitInterface()
-        var s_menuRetrofitInterface = RetrofitClient.getS_MenuRetrofitInterface()
-        var orderRetrofitInterface = RetrofitClient.getOrderRetrofitInterface()
-        var cartRetrofitInterface = RetrofitClient.getCartRetrofitInterface()
-
-
-        //(1) Manager_add 페이지 [프론트 아직 완성 X]
-        getManagerApiBtn = findViewById(R.id.getApiButton) as android.widget.Button?
-        var foodnameText = findViewById(R.id.getName) as TextView //음식 메뉴
-        var foodpriceText = findViewById(R.id.getPrice) as TextView //음식 가격
-
-        getApiBtn?.setOnClickListener {
-            callSMenu = orderRetrofitInterface.user
-            (callSMenu as Call<S_Menu>?)?.clone()?.enqueue(object : Callback<S_Menu> {
-                override fun onResponse(call: Call<S_Menu>, response: Response<S_Menu>) {
-                    if (response.isSuccessful) {
-                        foodnameText.text = response.body()!!.SMenu_NM
-                        foodpriceText.text = Integer.toString(response.body()!!.SMenu_PR)
-                    }
-                }
-
-                override fun onFailure(call: Call<S_Menu>, t: Throwable) {
-                    Log.e("retrofit 연동", "실패")
-                    t.printStackTrace()
-                }
-            })
-
 
     }
 
