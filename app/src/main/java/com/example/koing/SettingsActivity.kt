37d4b8c1e009f7d3215d12722962b15389f70b9a
jaehyun <hyun18_@naver.com>
2022-11-13 21:50:38 +0900

    환경설정 페이지 수정

--- app/src/main/java/com/example/koing/SettingsActivity.kt
+++ app/src/main/java/com/example/koing/SettingsActivity.kt
@@ -1,16 +1,73 @@
 package com.example.koing
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.TextUtils.replace
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import com.example.koing.databinding.ActivitySettingsBinding
+import androidx.appcompat.widget.Toolbar
+import androidx.preference.ListPreference
+import androidx.preference.PreferenceFragmentCompat
+import com.example.koing.databinding.ActivityFavoriteBinding
+import com.example.koing.databinding.SettingsActivityBinding
 
 class SettingsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        setContentView(R.layout.settings_activity)
 
-        val binding = ActivitySettingsBinding.inflate(layoutInflater)
-        val context = this
+        setSupportActionBar(findViewById(R.id.settings_toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.settings, SettingsFragment(preferenceChangeListener))
+            .commit()
+    }
+
+    val preferenceChangeListener: SharedPreferences.OnSharedPreferenceChangeListener =
+        SharedPreferences.OnSharedPreferenceChangeListener { var1, key ->
+            when (key){
+                "test1", "test2" -> {
+                    val intent: Intent = getIntent()
+                    finish()
+                    startActivity(intent)
+                }
+                else -> {
+                }
+            }
+        }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+            else -> {}
+        }
+        return super .onOptionsItemSelected(item)
+    }
+
+    class SettingsFragment(val listener: SharedPreferences.OnSharedPreferenceChangeListener) : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            findPreference<ListPreference>("test1")?.setOnPreferenceChangeListener { preference, newValue ->
+                return@setOnPreferenceChangeListener true
+            }
+        }
+
+        override fun onResume() {
+            super.onResume()
+            preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(listener)
+        }
+
+        override fun onPause() {
+            super.onPause()
+            preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(listener)
+        }
     }
 }
